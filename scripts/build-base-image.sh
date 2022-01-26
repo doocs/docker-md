099294c28da6416098a4bead6a572454e9a1820f
soulteary <soulteary@gmail.com>
2022-01-26 22:33:59 +0800

    chore: optimize build scripts

--- scripts/build-base-image.sh
+++ scripts/build-base-image.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
 RELEASE_DIR='./docker';
-REPO_NAME='soulteary/doocs-md'
+REPO_NAME='doocs/md'
 
 for app_ver in $RELEASE_DIR/*; do
-    tag=$(echo $app_ver | cut -b 10-);
-    echo "Build: $tag";
-    set -a
-        . "$app_ver/.env"
-    set +a
 
     if [ -f "$app_ver/Dockerfile.base" ]; then
+
+        tag=$(echo $app_ver | cut -b 10-);
+        echo "Build: $tag";
+        set -a
+            . "$app_ver/.env"
+        set +a
 
         echo $app_ver
         echo "VER_APP: $VER_APP"
@@ -18,7 +19,7 @@ for app_ver in $RELEASE_DIR/*; do
         echo "VER_GOLANG: $VER_GOLANG"
         echo "VER_ALPINE: $VER_ALPINE"
 
-        docker build --build-arg VER_APP=$VER_APP -f "$app_ver/Dockerfile.base" -t "doocs/md:${VER_APP}-assets" "$app_ver"
+        docker build --build-arg VER_APP=$VER_APP -f "$app_ver/Dockerfile.base" -t "$REPO_NAME:${VER_APP}-assets" "$app_ver"
     fi
 
 done
