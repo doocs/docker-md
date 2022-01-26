d470c8469b707cc890d042b384e1662c584e4104
soulteary <soulteary@gmail.com>
2022-01-26 22:49:20 +0800

    feat: add docker push script for release

---
+++ scripts/push-images.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+RELEASE_DIR='./docker';
+REPO_NAME='doocs/md'
+
+for app_ver in $RELEASE_DIR/*; do
+
+    tag=$(echo $app_ver | cut -b 10-);
+
+    if [ -f "$app_ver/Dockerfile.base" ]; then
+        # 推送构建产物，方便其他的用户和爱好者进行二次封装
+        docker push "$REPO_NAME:${VER_APP}-assets"
+    fi
+
+    if [ -f "$app_ver/Dockerfile.standalone" ]; then
+        # 推送单个二进制的镜像
+        docker push "$REPO_NAME:${VER_APP}"
+    fi
+
+    if [ -f "$app_ver/Dockerfile.nginx" ]; then
+        # 推送使用 Nginx 的镜像
+        docker push "$REPO_NAME:${VER_APP}-nginx"
+    fi
+
+done
